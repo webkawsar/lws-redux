2083c4fe2931942fb366051a2c3f1b75d0aa7328
webkawsar <web.kawsarahmed@gmail.com>
2023-02-20 16:28:14 +0600

    practice 2.3, 2.4 creating store and connect API

---
+++ module_2/counter-application/src/components/Counter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { decrement, increment } from '../redux/counter/actions';
+
+// Higher order component
+// A Higher order component is a function that takes a component as parameter and returns a new component
+
+
+const Counter = ({count}) => {
+
+
+  return (
+    <div>Counter</div>
+  )
+}
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        count: state.value
+    }
+}
+
+const mapDIspatchToProps = (dispatch) => {
+    return {
+        increment: (value) => dispatch(increment(value)),
+        decrement: (value) => dispatch(decrement(value)),
+    }
+}
+
+export default connect(mapStateToProps, mapDIspatchToProps)(Counter);
