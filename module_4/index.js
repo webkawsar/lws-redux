0b916918818c349c6614402b862cf427d6a912cf
webkawsar <web.kawsarahmed@gmail.com>
2023-03-01 11:23:46 +0600

    basic asynchronus action

---
+++ module_4/index.js
@@ -0,0 +1,55 @@
+const { createStore, applyMiddleware } = require("redux");
+const { delayActionMiddleware } = require("./middlewares");
+
+// initial state
+const initialState = {
+    todos: []
+};
+
+// reducer
+const todoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "todos/todoAdded":
+            return {
+                ...state,
+                todos: [
+                    ...state.todos,
+                    {
+                        title: action.payload
+                    }
+                ]
+            }
+
+        case "todos/todoLoaded":
+            return {
+                ...state,
+                todos: [ ...state.todos, ...action.payload ]
+            }
+    
+        default:
+            return state;
+    }
+}
+
+// store
+
+const store = createStore(todoReducer, applyMiddleware(delayActionMiddleware));
+
+
+// subscribe to state changes
+store.subscribe(() => {
+
+    console.log(store.getState());
+})
+
+
+// action dispatch
+store.dispatch({
+    type: "todos/todoAdded",
+    payload: 'Learn Redux from LWS'
+})
+
+
+
+
+
