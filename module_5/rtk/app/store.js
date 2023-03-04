e6f1c4d6b4393bddada245c06c5dd299ec946d10
webkawsar <web.kawsarahmed@gmail.com>
2023-03-04 19:24:56 +0600

    createSlice and configure store

---
+++ module_5/rtk/app/store.js
@@ -0,0 +1,12 @@
+const { configureStore } = require("@reduxjs/toolkit");
+const counterReducer = require('../features/counter/counterSlice')
+
+
+// configure store
+const store = configureStore({
+    reducer: {
+        counter: counterReducer
+    }
+})
+
+module.exports = store;
