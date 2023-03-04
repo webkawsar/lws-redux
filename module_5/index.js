c6effa6f72a299340e2839b45b552468fdad5107
webkawsar <web.kawsarahmed@gmail.com>
2023-03-04 20:00:39 +0600

    action dispatch

--- module_5/index.js
+++ module_5/index.js
@@ -1,8 +1,15 @@
+const store = require("./rtk/app/store");
+const { counterActions } = require("./rtk/features/counter/counterSlice");
 
 
 
+// subscribe o state changes
+store.subscribe(() => {
+    console.log(store.getState());
+})
 
 
-
-
+store.dispatch(counterActions.increment());
+store.dispatch(counterActions.increment());
+store.dispatch(counterActions.decrement());
 
