fcfeb42347f70ef59a54cfd288f51c90f2c62ab9
webkawsar <web.kawsarahmed@gmail.com>
2023-03-08 16:45:23 +0600

    show tag in UI

--- module_6/client/src/components/tags/Tags.js
+++ module_6/client/src/components/tags/Tags.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTags } from "../../features/tags/tagsSlice";
 import Tag from "./Tag";
 
 const Tags = () => {
-  return (
-    <section>
-      <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
-        <Tag />
+  const { tags } = useSelector((state) => state.tags);
+  const dispatch = useDispatch();
 
-        {/* <div
-                    className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
-                >
-                    redux
-                </div> */}
-      </div>
-    </section>
+  useEffect(() => {
+    dispatch(fetchTags());
+  }, [dispatch]);
+
+  return (
+    <>
+      {tags.length && (
+        <section>
+          <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto">
+            {
+              tags.map(tag => <Tag tag={tag} key={tag.id} />)
+            }
+
+            {/* <div
+                      className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
+                  >
+                      redux
+                  </div> */}
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
