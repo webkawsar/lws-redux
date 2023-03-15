2d91473c1b91f49e80b923b3d5cc28663388222f
webkawsar <web.kawsarahmed@gmail.com>
2023-03-15 20:23:54 +0600

    edit video api

--- module_8/src/components/edit/Form.js
+++ module_8/src/components/edit/Form.js
@@ -1,54 +1,143 @@
+// import Success from "../ui/Success";
+import { useState } from "react";
+import { useEditVideoMutation } from "../../features/api/apiSlice";
+import Success from "../ui/Success";
 import TextArea from "../ui/TextArea";
 import TextInput from "../ui/TextInput";
 
-export default function Form() {
-    return (
-        <form action="#" method="POST">
-            <div className="shadow overflow-hidden sm:rounded-md">
-                <div className="px-4 py-5 bg-white sm:p-6">
-                    <div className="grid grid-cols-6 gap-6">
-                        <div className="col-span-6 sm:col-span-3">
-                            <TextInput title="Video Title" />
-                        </div>
+const initialData = {
+  title: "",
+  author: "",
+  description: "",
+  link: "",
+  thumbnail: "",
+  date: "",
+  duration: "",
+  views: "",
+};
 
-                        <div className="col-span-6 sm:col-span-3">
-                            <TextInput title="Author" />
-                        </div>
+export default function Form({ video }) {
+  const [inputData, setInputData] = useState({ ...video });
+  const [editVideo, { data: editedVideo, isLoading, isSuccess, isError, error }] =
+  useEditVideoMutation();
 
-                        <div className="col-span-6">
-                            <TextArea title="Description" />
-                        </div>
+  const handleChange = (e) => {
+    setInputData({
+      ...inputData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-                        <div className="col-span-6">
-                            <TextInput title="YouTube Video link" />
-                        </div>
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                        <div className="col-span-6">
-                            <TextInput title="Thumbnail link" />
-                        </div>
+    editVideo({id: video?.id, data: inputData})    
+  };
 
-                        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                            <TextInput title="Upload Date" />
-                        </div>
+  const { title, author, description, link, thumbnail, date, duration, views } =
+    inputData;
 
-                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <TextInput title="Video Duration" />
-                        </div>
 
-                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <TextInput title="Video no of views" />
-                        </div>
-                    </div>
-                </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button
-                        type="submit"
-                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"
-                    >
-                        Save
-                    </button>
-                </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="shadow overflow-hidden sm:rounded-md">
+        <div className="px-4 py-5 bg-white sm:p-6">
+          <div className="grid grid-cols-6 gap-6">
+            <div className="col-span-6 sm:col-span-3">
+              <TextInput
+                title="Video Title"
+                name="title"
+                onChange={handleChange}
+                value={title}
+                required
+              />
             </div>
-        </form>
-    );
+
+            <div className="col-span-6 sm:col-span-3">
+              <TextInput
+                title="Author"
+                name="author"
+                onChange={handleChange}
+                value={author}
+                required
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextArea
+                title="Description"
+                name="description"
+                onChange={handleChange}
+                value={description}
+                required
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextInput
+                title="YouTube Video link"
+                name="link"
+                onChange={handleChange}
+                value={link}
+                required
+              />
+            </div>
+
+            <div className="col-span-6">
+              <TextInput
+                name="thumbnail"
+                title="Thumbnail link"
+                onChange={handleChange}
+                value={thumbnail}
+                required
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+              <TextInput
+                title="Upload Date"
+                name="date"
+                type="date"
+                onChange={handleChange}
+                value={date}
+                required
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+              <TextInput
+                title="Video Duration"
+                name="duration"
+                onChange={handleChange}
+                value={duration}
+                required
+              />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+              <TextInput
+                title="Video no of views"
+                name="views"
+                onChange={handleChange}
+                value={views}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button
+            type="submit"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-0 focus:ring-offset-0 focus:ring-indigo-500"
+            disabled={isLoading}
+          >
+            Update
+          </button>
+        </div>
+
+        {isSuccess && <Success message="Video was edited successfully" />}
+        {isError && <Success message={error.message} />}
+      </div>
+    </form>
+  );
 }
--- module_8/src/features/api/apiSlice.js
+++ module_8/src/features/api/apiSlice.js
@@ -1,43 +1,79 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    reducerPath: 'lwsApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:9000'
-    }),
-    tagTypes: ['Videos'],
-    endpoints: (builder) => {
-        return {
-            getVideos: builder.query({
-                query: () => '/videos',
-                keepUnusedDataFor: 5,
-                providesTags: ["Videos"]
-            }),
-            getVideo: builder.query({
-                query: (videoId) => `/videos/${videoId}`
-            }),
-            getRelatedVideos: builder.query({
-                query: ({id, title}) => {
-                    const tags = title.split(' ');
-                    const likes = tags.map(tag => `title_like=${tag}`);
-                    const queryString = `/videos?${likes.join('&')}&_limit=4`;
-                    return queryString;
-                }
-            }),
-            addVideo: builder.mutation({
-                query: (data) => {
-                    return {
-                        url: '/videos',
-                        method: 'POST',
-                        body: data
-                    }
-                },
-                invalidatesTags: ['Videos']
-            })
-        }
-    }
-})
+  reducerPath: "lwsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:9000",
+  }),
+  tagTypes: ["Videos", "Video", "RelatedVideo"],
+  endpoints: (builder) => {
+    return {
+      getVideos: builder.query({
+        query: () => "/videos",
+        keepUnusedDataFor: 5,
+        providesTags: ["Videos"],
+      }),
+      getVideo: builder.query({
+        query: (videoId) => `/videos/${videoId}`,
+        providesTags: (result, error, arg) => [
+          {
+            type: "Video",
+            id: arg,
+          },
+        ],
+      }),
+      getRelatedVideos: builder.query({
+        query: ({ id, title }) => {
+          const tags = title.split(" ");
+          const likes = tags.map((tag) => `title_like=${tag}`);
+          const queryString = `/videos?${likes.join("&")}&_limit=4`;
+          return queryString;
+        },
+        providesTags: (result, error, arg) => [
+            {
+              type: "RelatedVideo",
+              id: arg.id,
+            },
+          ],
+      }),
+      addVideo: builder.mutation({
+        query: (data) => {
+          return {
+            url: "/videos",
+            method: "POST",
+            body: data,
+          };
+        },
+        invalidatesTags: ["Videos"],
+      }),
+      editVideo: builder.mutation({
+        query: ({ id, data }) => {
+          return {
+            url: `/videos/${id}`,
+            method: "PATCH",
+            body: data,
+          };
+        },
+        invalidatesTags: (result, error, arg) => [
+          "Videos",
+          {
+            type: "Video",
+            id: arg.id,
+          },
+          {
+            type: "RelatedVideo",
+            id: arg.id,
+          },
+        ],
+      }),
+    };
+  },
+});
 
-
-export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery, useAddVideoMutation } = apiSlice;
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetRelatedVideosQuery,
+  useAddVideoMutation,
+  useEditVideoMutation,
+} = apiSlice;
