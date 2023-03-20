8ac65c0449f3bcb521ee8490f8354cb690565036
webkawsar <web.kawsarahmed@gmail.com>
2023-03-20 08:39:23 +0600

    log in persistency

--- module_9/src/App.js
+++ module_9/src/App.js
@@ -1,20 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import useAuthCheck from "./hooks/useAuthCheck";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/inbox" element={<Conversation />} />
-                <Route path="/inbox/:id" element={<Inbox />} />
-            </Routes>
-        </Router>
-    );
+  const authChecked = useAuthCheck();
+
+  return !authChecked ? (
+    <div>Checking authentication....</div>
+  ) : (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Conversation />} />
+        <Route path="/inbox/:id" element={<Inbox />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- module_9/src/features/api/apiSlice.js
+++ module_9/src/features/api/apiSlice.js
@@ -1,10 +1,25 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_API_URL
+        baseUrl: process.env.REACT_APP_API_URL,
+        prepareHeaders: async (headers, {getState, endpoint}) => {
+
+            console.log(headers, 'headers')
+            console.log(getState, 'getState')
+            console.log(endpoint, 'endpoint')
+
+            const token = getState()?.auth?.accessToken;
+            if(token) {
+                headers.set('Authorization', `Bearer token ${token}`)
+            }
+
+            console.log(headers, 'headers')
+
+            return headers;
+        }
     }),
     tagTypes: [],
     endpoints: (builder) => ({})
