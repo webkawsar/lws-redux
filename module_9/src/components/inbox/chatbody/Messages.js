7d5f027f643c385185555825ff7c247360e464de
webkawsar <web.kawsarahmed@gmail.com>
2023-03-20 18:32:50 +0600

    messages API and functionalities

--- module_9/src/components/inbox/chatbody/Messages.js
+++ module_9/src/components/inbox/chatbody/Messages.js
@@ -1,28 +1,19 @@
+import { useSelector } from "react-redux";
 import Message from "./Message";
 
-export default function Messages() {
-    return (
-        <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
-            <ul className="space-y-2">
-                <Message justify="start" message="Hjdjd" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-                <Message justify="start" message="Hi" />
-                <Message justify="start" message="How are you?" />
-                <Message justify="end" message="I am fine what about you?" />
-            </ul>
-        </div>
-    );
+export default function Messages({ messages }) {
+  const { user } = useSelector((state) => state.auth);
+
+  return (
+    <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
+      <ul className="space-y-2">
+        {messages.slice().sort((a, b) => a.timestamp - b.timestamp).map((message) => {
+          const { id, message: lastMessage, sender } = message;
+
+          const justify = sender.email !== user?.email ? "start" : "end";
+          return <Message key={id} justify={justify} message={lastMessage} />;
+        })}
+      </ul>
+    </div>
+  );
 }
