d45bb17c67fbb68ef7135237bbee90cf36a550ec
webkawsar <web.kawsarahmed@gmail.com>
2023-03-20 09:24:49 +0600

    added public/private route authenc=tication

--- module_9/src/hooks/useAuthCheck.js
+++ module_9/src/hooks/useAuthCheck.js
@@ -6,26 +6,21 @@ const useAuthCheck = () => {
   const dispatch = useDispatch();
   const [authChecked, setAuthChecked] = useState(false);
 
-
   useEffect(() => {
-
     const localAuth = localStorage?.getItem("auth");
     if (localAuth) {
-
       const auth = JSON.parse(localAuth);
       if (auth?.accessToken && auth?.user) {
-        
         dispatch(
           userLoggedIn({
             accessToken: auth.accessToken,
             user: auth.user,
           })
         );
-
-        setAuthChecked(true);
       }
     }
-    
+
+    setAuthChecked(true);
   }, [dispatch, setAuthChecked]);
 
   return authChecked;
